02857de1c6004e01e48a8e1e292d630ccafe58f7
Kirill Kurbatov <kirillkurbatov482.com>
2023-08-21 01:39:17 +0300

    new file

--- .folder/итоговая работа.py
+++ .folder/итоговая работа.py
@@ -1,34 +1,18 @@
+import pandas as pd 
+import numpy as np 
+import random
+ 
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+ 
+#==================================================#
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
 
-# 2
-# 3
-# 4
-# 5
-# 6
-# 7
-# 8
-# 9
-# 10
-# 11
-# 12
-# 13
-# 14
-# 15
-# 16
-# 17
-# import pandas as pd 
-# import numpy as np 
-# import random
- 
-# lst = ['robot'] * 10
-# lst += ['human'] * 10
-# random.shuffle(lst)
-# data = pd.DataFrame({'whoAmI': lst})
-# print(data)
- 
-# #==================================================#
-# data['tmp'] = 1
-# data.set_index([data.index, 'whoAmI'], inplace=True)
-# data = data.unstack(level=-1, fill_value = 0).astype(int)
-# data.columns = data.columns.droplevel()
-# data.columns.name = None
-# print(data)
